ce57c32a4e73c0a2e7a22d20b583caa6ae207983
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2024-10-05 23:51:03 +0200

    feat: everything in one page

--- trixelart/app.py
+++ trixelart/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, send_file, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 import requests
 from PIL import Image
 from io import BytesIO
 from image_processing import pixelate_image_with_right_triangles
 import base64
+
 app = Flask(__name__)
 
 def open_image_from_url(url):
@@ -15,12 +16,14 @@ def open_image_from_url(url):
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    pixelated_img_data = request.args.get('pixelated_img_data')
+    url = request.args.get('url')
+    return render_template('index.html', pixelated_img_data=pixelated_img_data, url=url)
 
 @app.route('/pixelate', methods=['POST'])
 def pixelate():
     url = request.form.get('url')
-    triangle_size = int(request.args.get('size', 30))
+    triangle_size = int(request.form.get('size', 30))
     if not url:
         return "URL parameter is required", 400
     
@@ -31,7 +34,7 @@ def pixelate():
         pixelated_img.save(img_io, 'PNG')
         img_io.seek(0)
         pixelated_img_data = base64.b64encode(img_io.getvalue()).decode('utf-8')
-        return render_template('result.html', pixelated_img_data=pixelated_img_data)
+        return redirect(url_for('index', pixelated_img_data=pixelated_img_data, url=url))
     except Exception as e:
         return str(e), 500
 
